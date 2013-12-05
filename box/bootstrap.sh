eb8a15dbce8320eaaacbf6c15acc6d7fbc050ca3
Jeroen Janssens <jeroen@jeroenjanssens.com>
2013-12-05 07:28:50 -0500

    Add Vagrant environment. (Blog post about this is coming soon.)

---
+++ box/bootstrap.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+HOME=/home/vagrant
+REPOS=$HOME/repos
+TOOLS=$HOME/tools
+DOWNLOADS=$HOME/downloads
+
+log () {
+	echo "[data-science-toolbox] $1" | tee -a $HOME/bootstrap.log
+}
+
+mkdir -p $REPOS
+mkdir -p $TOOLS
+mkdir -p $DOWNLOADS
+
+log 'Adding PPA for node...'
+sudo add-apt-repository -y ppa:chris-lea/node.js
+
+log 'Retrieving updated list of packages...'
+apt-get update
+
+log 'Installing common requirements...'
+apt-get install -y curl git python-pip make python-dev
+
+log 'Installing GNU parallel...'
+cd $DOWNLOADS
+wget http://ftp.gnu.org/gnu/parallel/parallel-latest.tar.bz2
+tar -xjf parallel-latest.tar.bz2
+cd $(find -name 'parallel-*' -type d)
+./configure && make && sudo make install
+
+log 'Installing jq...'
+cd $TOOLS
+wget http://stedolan.github.io/jq/download/linux64/jq
+chmod +x jq
+
+log 'Installing requirements for scrape...'
+sudo apt-get install -y libxml2-dev libxslt1-dev 
+sudo pip install cssselect lxml
+
+log 'Cloning data-science-toolbox repository...'
+cd $REPOS
+git clone https://github.com/jeroenjanssens/data-science-toolbox.git
+
+log 'Installing csvkit...'
+sudo pip install csvkit
+
+cd $HOME
+log 'Installing json2csv...'
+echo 'golang-go golang-go/dashboard boolean true' > preseed.conf
+sudo debconf-set-selections preseed.conf
+sudo apt-get install -y golang-go
+sudo go get github.com/jehiah/json2csv
+
+log 'Installing xml2json...'
+sudo apt-get install -y python-software-properties python g++ make 
+sudo apt-get install -y nodejs
+curl https://npmjs.org/install.sh | sudo clean=yes sh
+sudo npm install -g xml2json-command
+
+sudo echo 'export PATH=$PATH:/usr/lib/go/bin:/home/vagrant/repos/data-science-toolbox/tools:/home/vagrant/tools' >> $HOME/.bashrc 
+sudo chown -R vagrant:vagrant $REPOS
+sudo chown -R vagrant:vagrant $TOOLS
+sudo rm -rf $DOWNLOADS
+sudo rm -rf $HOME/tmp
+sudo rm $HOME/preseed.conf
+
+log 'Finished bootstrapping the Data Science Toolbox'
