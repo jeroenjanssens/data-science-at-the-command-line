2a2a3e334a5c8a188d5a097a5850fa576c52b851
Jeroen Janssens <jeroen@jeroenjanssens.com>
2014-07-20 17:02:39 -0400

    update bundle

--- dst/build/provision.sh
+++ dst/build/provision.sh
@@ -2,4 +2,4 @@
 PLAYBOOK="$1"
 shift
 ARGS="$@"
-/usr/local/bin/ansible-playbook bundle/${PLAYBOOK}.yml -c local -i '127.0.0.1,' --extra-vars="dst_username=vagrant" "$ARGS"
+/usr/local/bin/ansible-playbook bundle/${PLAYBOOK}.yml -c local -i '127.0.0.1,' --extra-vars="dst_username=vagrant" $ARGS
