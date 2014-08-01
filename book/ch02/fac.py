3b46433eff939b478ae789f84196f4fc585f9c1e
Jeroen Janssens <jeroen@jeroenjanssens.com>
2014-07-31 21:35:53 -0400

    add iris dataset to chapter 5

---
+++ book/ch02/fac.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def factorial(x):
+    result = 1
+    for i in xrange(2, x + 1):
+        result *= i
+    return result
+
+if __name__ == "__main__":
+    import sys
+    x = int(sys.argv[1])
+    print factorial(x)
