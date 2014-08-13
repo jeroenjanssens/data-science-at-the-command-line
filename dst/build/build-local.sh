c3b9221398a372b0ca525eb6a60b54c0d2ff6271
Jeroen Janssens <jeroen@jeroenjanssens.com>
2014-08-13 17:02:50 -0400

    add imdb data set

--- dst/build/build-local.sh
+++ dst/build/build-local.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-DST_DSATCL_VERSION="0.0.2"
+DST_DSATCL_VERSION="0.0.5"
+DST_DSATCL_BOX="dst-dsatcl-${DST_DSATCL_VERSION}.box"
+DST_DSATCL_URL="https://data-science-toolbox.s3.amazonaws.com/${DST_DSATCL_BOX}"
 
-rm -rf output-virtualbox-iso
-rm -rf packer_virtualbox-iso_virtualbox.box
+#echo "Build Vagrant with Packer"
+#rm -rf output-virtualbox-iso
+#rm -rf packer_virtualbox-iso_virtualbox.box
+#packer build -var-file=variables.json -var "dst_dsatcl_version=${DST_DSATCL_VERSION}" -only=virtualbox-iso dst-dsatcl.json && mv packer_virtualbox-iso_virtualbox.box boxes/$DST_DSATCL_BOX
 
-echo "Build Vagrant with Packer"
-packer build -var-file=variables.json -var "dst_dsatcl_version=${DST_DSATCL_VERSION}" -only=virtualbox-iso dst-dsatcl.json 
+#echo "Update local Vagrant box"
+#vagrant box remove dst-dsatcl
+#vagrant box add dst-dsatcl boxes/$DST_DSATCL_BOX
 
-echo "Rename Vagrant box"
-mv packer_virtualbox-iso_virtualbox.box boxes/$DST_DSATCL_BOX
+echo $DST_DSATCL_URL
 
-echo "Update local Vagrant box"
-vagrant box remove dst-dsatcl
-vagrant box add dst-dsatcl boxes/$DST_DSATCL_BOX
+#echo "Upload to S3"
+#aws s3 cp boxes/$DST_DSATCL_BOX s3://data-science-toolbox/$DST_DSATCL_BOX --acl public-read
