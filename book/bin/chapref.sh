07d6f534ea450bb4e580ffd36c08f63f311448ba
Jeroen Janssens <jeroen@jeroenjanssens.com>
2018-01-09 15:01:37 +0100

    After reference update

--- book/bin/chapref.sh
+++ book/bin/chapref.sh
@@ -4,17 +4,17 @@ function chapref() {
 
 function allchaprefs() {
     cat $1 |
-    sed -re 's/\[(Chapter [0-9]{1,2})\]\([a-z\-]+\)/\1/g' |
-    chapref "1" "chapter-introduction" |
-    chapref "2" "chapter-getting-started" |
-    chapref "3" "chapter-obtaining-data" |
-    chapref "4" "chapter-creating-reusable-command-line-tools" |
-    chapref "5" "chapter-scrubbing-data" |
-    chapref "6" "chapter-managing-your-data-workflow" |
-    chapref "7" "chapter-exploring-data" |
-    chapref "8" "chapter-parallel-pipelines" |
-    chapref "9" "chapter-modeling-data" |
-    chapref "10" "chapter-conclusion" > $1.bak
+    sed -re 's/\[(Chapter [0-9]{1,2})\]\(#[a-z0-9\-]+\)/\1/g' |
+    chapref "1" "chapter-1-introduction" |
+    chapref "2" "chapter-2-getting-started" |
+    chapref "3" "chapter-3-obtaining-data" |
+    chapref "4" "chapter-4-creating-reusable-command-line-tools" |
+    chapref "5" "chapter-5-scrubbing-data" |
+    chapref "6" "chapter-6-managing-your-data-workflow" |
+    chapref "7" "chapter-7-exploring-data" |
+    chapref "8" "chapter-8-parallel-pipelines" |
+    chapref "9" "chapter-9-modeling-data" |
+    chapref "10" "chapter-10-conclusion" > $1.bak
     mv $1.bak $1
 }
 
@@ -22,16 +22,16 @@ function addid() {
     sed -i -re '1s/^# ([^{]+)(\{.*$)?/# \1{#'$2'}/' $1.Rmd
 }
 
-addid "01" "chapter-introduction"
-addid "02" "chapter-getting-started"
-addid "03" "chapter-obtaining-data"
-addid "04" "chapter-creating-reusable-command-line-tools"
-addid "05" "chapter-scrubbing-data"
-addid "06" "chapter-managing-your-data-workflow"
-addid "07" "chapter-exploring-data"
-addid "08" "chapter-parallel-pipelines"
-addid "09" "chapter-modeling-data"
-addid "10" "chapter-conclusion"
+addid "01" "chapter-1-introduction"
+addid "02" "chapter-2-getting-started"
+addid "03" "chapter-3-obtaining-data"
+addid "04" "chapter-4-creating-reusable-command-line-tools"
+addid "05" "chapter-5-scrubbing-data"
+addid "06" "chapter-6-managing-your-data-workflow"
+addid "07" "chapter-7-exploring-data"
+addid "08" "chapter-8-parallel-pipelines"
+addid "09" "chapter-9-modeling-data"
+addid "10" "chapter-10-conclusion"
 
 allchaprefs "01.Rmd"
 allchaprefs "02.Rmd"
