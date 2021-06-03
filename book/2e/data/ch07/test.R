22607e211e688f6174dfac57ee6843d512b6d63e
Jeroen Janssens <jeroen@jeroenjanssens.com>
2021-06-03 20:51:00 +0200

    Chapter 7 cleanup and screenshots

--- book/2e/data/ch07/test.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-library(ggplot2)
-df <- janitor::clean_names(readr::read_delim(file("stdin", "rb", raw = TRUE), delim = ",", col_names = TRUE))
-p <- qplot(x = bill, y = tip, margins = FALSE, geom = "auto", color = sex, data = df)
-p + theme(legend.position = "bottom")
